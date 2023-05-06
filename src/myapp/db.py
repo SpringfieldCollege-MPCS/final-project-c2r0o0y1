a1cc1a878feb00891cc1c2e8d349e3ae8d145fa9
Chhandak Roy <roychhandak2001@gmail.com>
2023-05-05 23:39:51 -0400

    MODIFIED db.py and repl/.app.py

--- src/myapp/db.py
+++ src/myapp/db.py
@@ -1,3 +1,4 @@
+
 import pathlib
 from datetime import date
 from typing import Optional, List  # 
@@ -26,10 +27,35 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
 
 ###############################
 ### Start Model Definitions ###
-class Movie(SQLModel, table=True):  # 
-    id: Optional[int] = Field(default=None, primary_key=True)  # this will autoincrement by default
-    title: str
-    director: str 
+# class Movie(SQLModel, table=True):  # 
+#     id: Optional[int] = Field(default=None, primary_key=True)  # this will autoincrement by default
+#     title: str
+#     director: str 
+
+class Company(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+
+
+class User(SQLModel, table=True):  # 
+    id: Optional[int] = Field(default=None, primary_key=True) 
+    first_name: str
+    last_name: str
+    email: str
+    company_id: Optional[int] = Field(
+        sa_column=Column(Integer, ForeignKey("company.id", ondelete="CASCADE"))
+    )
+
+class Password(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    website: str
+    username: str
+    salted: str
+    created: str
+    updated: str
+    user_id: Optional[int] = Field(
+        sa_column=Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
+    )
 
 
 ### End Model Definitions ####
@@ -38,18 +64,49 @@ class Movie(SQLModel, table=True):  #
 
 ##################################
 ### Begin Function Definitions ###
-def create_movie(title:str, director:str, save=True):
-    movie = Movie(title=title, director=director)
+def create_company(name:str=None, save=True):
+    company = Company(name=name)
     if save:
         with Session(engine) as session:
-            session.add(movie)
+            session.add(company)
             session.commit()
-            session.refresh(movie)
-    return movie
+            session.refresh(company)
+    return company
 
-def get_movies() -> List[Movie]:
+def create_user(first_name:str, last_name:str=None, email:str=None, company_id:Optional[int]=None , save=True):
+    user = User(first_name=first_name, last_name=last_name, email=email, company_id=company_id)
+    if save:
+        with Session(engine) as session:
+            session.add(user)
+            session.commit()
+            session.refresh(user)
+    return user
+
+def create_password(url:str=None, username:str=None, password:str=None, create:str=None, update:str=None, user_id:Optional[int]=None, save=True):
+    if create is None:
+        create = str(date.today())
+    if update is None:
+        update = str(date.today())
+    password = Password(website=url, username=username, salted=password, created=create, updated=update, user_id=user_id)
+    if save:
+        with Session(engine) as session:
+            session.add(password)
+            session.commit()
+            session.refresh(password)
+    return password
+
+
+def get_companies()->List[Company]:
     with Session(engine) as session:
-        return list(session.query(Movie).all())
+        return list(session.query(Company).all())
+
+def get_users(company_id=1) -> List[User]:
+    with Session(engine) as session:
+        return list(session.query(User).where(User.company_id == company_id))
+    
+def get_passwords(user_id=1) -> List[Password]:
+    with Session(engine) as session:
+        return list(session.query(Password).where(Password.user_id == user_id))
 
 ### End Function Definitions ###
 ################################
@@ -75,9 +132,15 @@ def delete_entity(entity):
 
 def create_fake_data():
     """Insert your fake data in here"""
-    create_movie("A tale of algorithms", "Jeremy Castagno", )
-    create_movie("How I made my first million","Manish Bhusal")
-    create_movie("Shrek", "Justin Manning")
+    # create_movie("A tale of algorithms", "Jeremy Castagno", )
+    # create_movie("How I made my first million","Manish Bhusal")
+    # create_movie("Shrek", "Justin Manning")
+    company_1 = create_company('Zomato')
+    company_2 = create_company('Flipkart')
+    user_1 = create_user('Chhandak', 'Roy','roychhandak2001@gmail.com',company_id=company_2.id)
+    user_2 = create_user('Akriti','Kaur','kaurakriti@gamil.com',company_id=company_1.id)
+    create_password("facebook.com",'cRoy','xxxxxxx','23-12-2012','26-12-2015',user_id=user_1.id)
+    create_password("github.com",'aKaur','yyyyyyyyy','23-12-2012','26-12-2015',user_id=user_2.id)
 
 def create_db_and_tables():  # 
     """This creates our tables and add some fake data"""
--- src/myapp/repl/app.py
+++ src/myapp/repl/app.py
@@ -5,44 +5,87 @@ from enum import Enum, auto
 from prompt_toolkit import PromptSession
 from sqlmodel import SQLModel
 
-from ..db import Movie, get_movies, delete_entity, get_entity, create_movie
+from ..db import Company,User,Password, get_companies, get_users,get_passwords,delete_entity, get_entity, create_company,create_user,create_password
 from .console import console
-from .helper import show_table_and_ask_for_command, Command, EntityNotFound
+from .helper import show_table_and_ask_for_command, Command, EntityNotFound, get_item
 
 class Step(Enum):
-    show_movies = auto()
+    show_companies = auto()
+    show_users = auto()
+    show_passwords = auto()
 
 @dataclass
 class AppState():
-    app_step:Step = Step.show_movies
-    movie_list = []
+    app_step:Step = Step.show_companies
+    all_company_list: List[Company] = field(default_factory=list)
+    all_user_list: List[User] = field(default_factory=list)
+    all_password_list: List[Password] = field(default_factory=list)
+    active_company: Optional[Company] = None
+    active_user: Optional[User] = None
+
     def __init__(self):
-        self.refresh_movies()
+        self.refresh_companies()
     
-    def refresh_movies(self):
-        self.movie_list = get_movies()
+    def refresh_companies(self):
+        self.all_company_list = get_companies()
+    
+    def refresh_users(self):
+        # self.all_user_list = get_users()
+        if self.active_company is not None:
+            self.all_user_list = get_users(self.active_company.id)
+        else:
+            self.all_user_list = []
+
+    def refresh_passwords(self):
+        # self.all_password_list = get_passwords()
+        if self.active_user is not None:
+            self.all_password_list = get_passwords(self.active_user.id)
+        else:
+            self.all_password_list = []
+    
+    def set_active_company(self, id:int):
+        self.active_company = get_item(id, self.all_company_list, model=Company)
+        self.refresh_users()
+
+    def set_active_user(self, id:int):
+        self.active_user = get_item(id, self.all_user_list, model=User)
+        self.refresh_passwords()
 
 def execute_command(session:PromptSession, state:AppState, state_key:str, model:SQLModel):
     
     response = show_table_and_ask_for_command(session, state, state_key, model)
+    if len(response.split(' ')) < 2:
+        # user wants to quit
+        if response == Command.quit:
+            return False # return false to make the program exit
+        # user just entered a bad command
+        # warn them and then loop again
+        console.print("[danger]You must type in a command and a value: Eg. 'select 1'")
+        return True
     if ' ' in response:
         command, value = response.split(' ', 1)
     else:
         command = response
         value = ""
     command = command.lower()
-    if command == Command.remove:
-        if model == Movie:
-            delete_entity(get_entity(Movie, int(value)))
-            state.refresh_movies()
-    elif command == Command.add:
-        if model == Movie:
-            title = session.prompt("Title: ")
-            director = session.prompt("Director: ")
-            create_movie(title=title, director=director)
-            state.refresh_movies()
+    if command == Command.select:
+        if model == Company:
+            state.set_active_company(value)
+            state.app_step = Step.show_users
+        elif model == User:
+            state.set_active_user(value)
+            state.app_step = Step.show_passwords
+
+    elif command == Command.remove:
+        if model == Company:
+            console.print("[warning]Not supported currently")
+        elif model == User:
+            if state.active_user is not None and state.active_user.id == int(value):
+                state.active_user = None
+                state.app_step = Step.show_users
+            delete_entity(get_entity(User, int(value)))
         else:
-            console.print("[danger]Not supported")
+            delete_entity(get_entity(Password, int(value)))
     elif command == Command.quit:
         return False
     else:
@@ -54,13 +97,17 @@ def cli():
     session = PromptSession() # allows us to prompt the user
 
     console.print("You can exit the program by pressing [success]CTRL+D[/success] at anytime")
-    console.print("You must type in a command and a value: Eg. 'select 1', 'complete 1'")
+    console.print("You must type in a command and a value: Eg. 'select _', 'remove _'")
     console.print()
     loop = True
     while loop:
         try:
-            if state.app_step == Step.show_movies:
-                loop = execute_command(session, state, 'movie_list', model=Movie)
+            if state.app_step == Step.show_companies:
+                loop = execute_command(session, state,'all_company_list', model=Company)
+            elif state.app_step == Step.show_users:
+                loop = execute_command(session, state, 'all_user_list', model=User)
+            elif state.app_step == Step.show_passwords:
+                loop = execute_command(session, state, 'all_password_list', model=Password)
         except KeyboardInterrupt:
             continue
         except EOFError:
